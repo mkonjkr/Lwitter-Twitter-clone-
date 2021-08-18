2a337408809a5d471715d8476feb4ef3313b38b0
unknown <mkonjkr@gmail.com>
2021-08-17 22:02:25 -0700

    #Base  Router setting

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
+
 import React from 'react';
 import ReactDOM from 'react-dom';
+import App from "./components/App.js";
+import firebase from "./firebase.js";
 
-import App from './App';
-
+console.log(firebase);
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
   document.getElementById('root')
-  
+
 );
